b721b02601262df20db7b47be98a9f3c0b29aaf2
Abdes <abdessalam.ouaazki@unine.ch>
2024-08-06 17:08:22 +0200

    Updated qualtrics link

--- code.js
+++ code.js
@@ -26,7 +26,7 @@
 	function redirectToQualtrics() {
 		if (typeof startTime !== 'undefined') {
 			let params = getSpecificQueryParams();
-			let baseUrl = 'https://neuchatel.eu.qualtrics.com/jfe/form/SV_aXk4LbBY9BVvNie';
+			let baseUrl = 'https://neuchatel.eu.qualtrics.com/jfe/form/SV_a8Iy0axKqHN0vMq';
 			let timeSpent = calculateTimeOnSite();
 			if (timeSpent > 10){
 				params['timeSpent'] = timeSpent;
